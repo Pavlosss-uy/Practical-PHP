3965366a2042ed230052e30738c34735b3c6cf57
Pavlos <youssefpavlos@gmail.com>
2024-07-31 10:52:58 +0300

    Editing Task1.2.5

--- Assignment1.2/Task1.2.5.php
+++ Assignment1.2/Task1.2.5.php
@@ -6,7 +6,7 @@ $tests=array(1,"tariq",1.5,true,7,'s',false);
 $testsLen = count($tests);
 
 for ($i=0;$i<$testsLen;$i++){
-    if ($tests[$i] == 'true'){
+    if (is_bool($tests[$i]) && $tests[$i] == 1){
         $tests[$i] = "yes";
     }elseif($tests[$i] == 0){
         $tests[$i] = "no";
